ff1909745377ec135e5dd465d5bda69d4b39b86e
gustavobarros24 <gmarinho2@gmail.com>
2024-10-12 15:09:28 +0100

    Thread

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,14 +1,36 @@
 package src.main;
 
 import javax.swing.JPanel;
+
+
+import java.awt.Color;
 import java.awt.Dimension;
 
-public class GamePanel extends JPanel {
 
-    public GamePanel() {
+public class GamePanel extends JPanel implements Runnable {
+
+    final int originalTileSize = 16;
+    final int scale = 2;
+    final int tileSize = originalTileSize * scale; //32x32 tile
+    final int maxScreenCol = 18;
+    final int maxScreenRow = 25;
+    Thread gameThread;
+    //final int screenWidth = tileSize * maxScreenCol; //768
+    //final int screenHeight = tileSize*maxScreenRow; //576
+
+    public GamePanel(){
         this.setPreferredSize(new Dimension(800, 600));
-        final int originalTileSize = 16;
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
     }
 
-    // Add other game-related methods here
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run(){
+        
+    }
 }
